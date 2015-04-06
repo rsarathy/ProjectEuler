fd438cee0c9b7cd3b7c0d1ad1523aef011cc7349
Rohit Sarathy <rohit@sarathy.org>
2015-04-06 03:43:03 -0500

    solved Problem 124

--- src/Problem124.java
+++ src/Problem124.java
@@ -1,6 +1,7 @@
+import java.util.Arrays;
 
 public class Problem124
-{
+{	
 	final static int MAX = 100000;
 	static int[] primes = ESieve.generatePrimes(MAX);
 	
@@ -17,6 +18,10 @@ public class Problem124
 	}
 
 	/**
+	 * Implements an array of "radicals" which are as a class,
+	 * defined as pairs of integers. See Radical.java for more information on
+	 * the defined class.
+	 * 
 	 * @param args
 	 */
 	public static void main(String[] args)
@@ -31,25 +36,20 @@ public class Problem124
 		for ( int i = 1; i < rads.length; i++ )
 			rads[i] = new Radical(i, rad(i));
 		
-		System.out.println(rads[504].toString());
-		
-		System.out.println(rads[3].toString());
-		System.out.println(rads[9].toString());
-		
-		System.out.println(rads[3].toString().compareTo(rads[9].toString()));
-		
-		Radical[] sorted = Radical.quickSort(rads);
-		System.out.println(sorted[10000].toString());
-//		for ( int i = 0; i < sort.length; i++ )
-//			if ( sort[i] == 3 )
-//				System.out.println(i);
+//		System.out.println(rads[504].toString());
 //		
-//		for ( int i = 0; i < 10; i++ )
-//			System.out.println((i+1) + " " + sort[i]);
-
+//		System.out.println(rads[3].toString());
+//		System.out.println(rads[9].toString());
+//		
+//		System.out.println(rads[3].compareTo(rads[9]));
+//		System.out.println(rads[8].compareTo(rads[9]));
+		
+		Arrays.sort(rads);
+		
+		System.out.println("\nE(10000) = " + rads[10000]);
+		
 		long endTime = System.currentTimeMillis();
 		System.out.println("The program took " + (endTime - startTime) + " ms to compile."); 
 
 	}
-
 }
