93b2a7d14483d1e21789e49a33882605e8010ede
Rohit Sarathy <rohit@sarathy.org>
2015-04-11 23:28:07 -0500

    problem 148 revisions

--- src/Problem148.java
+++ src/Problem148.java
@@ -19,36 +19,34 @@ public class Problem148
 		BigInteger seven = new BigInteger("7");
 		BigInteger zero = BigInteger.ZERO;
 
-		int rows = 100;
+		int rows = 344;
 		int ct = 0;
 
-		BigInteger[][] triangle = new BigInteger[100][100];
-		for (int i = 0; i < 100; i++)
-			for (int j = 0; j <= i; j++)
-			{
-				triangle[i][j] = Numbers.choose(i, j);
-				if (triangle[i][j].mod(seven).compareTo(zero) == 0)
-					ct++;
-			}
+		BigInteger[][] triangle = new BigInteger[rows][rows];
 
-		for (int i = 0; i < 100; i++)
+		for (int i = 0; i < rows; i++)
 		{
 			StringBuffer row = new StringBuffer();
 			for (int j = 0; j <= i; j++)
 			{
+				triangle[i][j] = Numbers.choose(i, j);
 				if (triangle[i][j].mod(seven).compareTo(zero) == 0)
+				{
 					row.append("0 ");
+					ct++;
+				}
 				else
 					row.append("1 ");
 			}
-			String s = spacing(99-i);
+			String s = spacing(rows-i-1);
 			String r = row.toString();
 			r = s + r;
 			System.out.println(r);
 		}
 
-		System.out.println("\n" + ((rows * rows + rows) / 2 - ct) + " / "
-				+ (rows * rows + rows) / 2 + " not divisible by 7.");
+		int triSize = (rows * rows + rows) / 2;
+		System.out.println("\n" + (triSize - ct) + " / " 
+				+ triSize + " not divisible by 7.");
 
 		long endTime = System.currentTimeMillis();
 		System.out.println("The program took " + (endTime - startTime)
