c46631a10632ebc2e1f73b28c4c0b38503ea794d
Rohit Sarathy <rohit@sarathy.org>
2015-05-19 00:22:28 -0700

    solved Problem 114

--- src/Problem114.java
+++ src/Problem114.java
@@ -1,39 +1,39 @@
 public class Problem114
 {
-	static int nthTri(int n)
+	static int corrFactor(int n)
 	{
-		return (n*n+n)/2;
+		int m = n % 6;
+		if ( m == 1 || m == 2 ) return -1;
+		if ( m == 0 || m == 3 ) return 0;
+		if ( m == 4 || m == 5 ) return 1;
+		
+		return -2; //unreachable, error catching
 	}
+	
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args)
 	{
 		long startTime = System.currentTimeMillis();
-		
-		int[] blocks = new int[51];
+				
+		long[] blocks = new long[51];
 		blocks[0] = 0;
-		blocks[1] = 0;
-		blocks[2] = 0;
+		blocks[1] = 1;
+		blocks[2] = 1;
 		blocks[3] = 2;
 		blocks[4] = 4;
 		blocks[5] = 7;
 		blocks[6] = 11;
 		blocks[7] = 17;
 		
-		for ( int n = 7; n <= 50; n++ )
-		{
-			int sumR_i = 0;
-			for ( int i = 3; i <= n-4; i++ )
-				sumR_i += blocks[i];
-			blocks[n] = nthTri(n-2) + sumR_i - n + 7;
-			
-			System.out.println("Number of ways to combine " + n + ": " + blocks[n]);
+		for ( int n = 8; n <= 50; n++ )
+		{			
+			blocks[n] = blocks[n-1] + blocks[n-2] + corrFactor(n);
+			System.out.println("Number of solutions to a row of length " + n + ": " + blocks[n]);
 		}
 		
 		long endTime = System.currentTimeMillis();
 		System.out.println("The program took " + (endTime - startTime) + " ms to compile."); 
-
 	}
-
 }
