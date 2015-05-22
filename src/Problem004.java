507598e81d058c15aa7e1a112535b50bee29c52d
Rohit Sarathy <rohit@sarathy.org>
2015-05-21 17:04:59 -0700

    Problem 4 fixes

--- src/Problem004.java
+++ src/Problem004.java
@@ -1,32 +1,13 @@
 public class Problem004
 {
-	public static int palindromify(int n)
-	{
-		int a = n / 100;
-		int b = ( n % 100 ) / 10;
-		int c = n - 100 * a - 10 * b;
-		
-		// System.out.println("a = "  + a + "\nb = " + b + "\nc = " + c);
-		
-		n = 100000 * a + 10000 * b + 1000 * c
-				+ 100 * c + 10 * b + a;
-		
-		return n;
+	public static boolean isPalindrome(String str)
+	{    
+	    int n = str.length();
+	    for (int i = 0; i < n/2; i++)
+	        if (str.charAt(i) != str.charAt(n-i-1)) 
+	        	return false;
+	    return true;    
 	}
-	
-	public static int divisor(int h)
-	{
-		for ( int div = 1; div < Math.sqrt(h) + 1; div++ )
-		{
-			int q = h / div;
-			if ( h % div == 0 && ( div >= 100 && div <= 999 ) && ( q >= 100 && q <= 999 ) )
-				return 1;			
-		}
-		
-		return -1;
-	}
-	
-	
 
 	/**
 	 * Largest palindrome that is the product of two 3-digit numbers.
@@ -39,18 +20,16 @@ public class Problem004
 	{
 		long startTime = System.currentTimeMillis();
 		
-		int[] palindromes = new int[900];
+		int maxPal = 0;
+		for ( int a = 101; a <= 999; a++ )
+			for ( int b = 100; b < a; b++ )
+			{
+				int ab = a*b;
+				if ( isPalindrome(ab+"") && ab > maxPal )
+					maxPal = ab;
+			}
 		
-		for ( int i = 0; i < palindromes.length; i++ )
-		{
-			palindromes[i] = i + 100;
-			palindromes[i] = palindromify(palindromes[i]);
-			if ( divisor(palindromes[i]) == 1 )
-				System.out.println(palindromes[i]);
-		}
-		
-		
-		// System.out.println(palindromes[438]);
+		System.out.println("Largest palindrome that is the product of two 3-digit numbers: " + maxPal);
 		
 		long endTime = System.currentTimeMillis();
 		System.out.println("The program took " + (endTime - startTime) + " ms to compile."); 
