07c639701973ca95d52cc7cdcc808f36b4bf3925
Rohit Sarathy <rohit@sarathy.org>
2015-06-02 21:02:21 -0700

    solved Problem 21

--- src/Problem021.java
+++ src/Problem021.java
@@ -2,31 +2,17 @@
 public class Problem021
 {
 
-	public static int sumDiv(int n)
+	static int sumDiv(int num)
 	{
 		int sum = 0;
-		
-		for ( int i = 1; i < Math.sqrt(n) + 1; i++ )
-			if ( n % i == 0 )
-				sum += (i + (n/i));
-		
-		return sum - n;
+		for (int i = 1; i < num; i++)
+			if (num % i == 0)
+				sum += i;
+		return sum;
 	}
-	
-	public static boolean isPrime(int n) 
-	{
-        if (n <= 1) return false;
-        if (n == 2) return true;
-        for (int i = 2; i <= Math.sqrt(n) + 1; i++)
-            if (n % i == 0)
-                return false;
-        return true;
-    }
-	
+
 	/**
 	 * Compute the sum of all amicable numbers < 10000.
-	 * 
-	 * 220 + 284 + 1184 + 1210 + 2620 + 2924 + 5020 + 5564 + 6232 + 6368
 	 */
 	public static void main(String[] args)
 	{
@@ -36,20 +22,17 @@ public class Problem021
 		
 		for ( int i = 1; i < 10000; i++ )
 		{
-			boolean amicable = sumDiv(i) == sumDiv(sumDiv(i)) 
-					&& i != sumDiv(sumDiv(i)) && !isPrime(i);
-			if ( amicable )
-			{
-				System.out.println(i);
+			int a1 = sumDiv(i);
+			int a2 = sumDiv(a1);
+			
+			if ( i == a2 )
 				amSum += i;
-			}
 		}
-
-		// System.out.println(sumDiv(28));
-//		System.out.println( sumDiv(220));
-//		System.out.println( sumDiv(sumDiv(220)));
+		
+		amSum -= (6 + 28 + 496 + 8128); //subtract all perfect numbers
+		
 		System.out.println(amSum);
-
+		
 		long endTime = System.currentTimeMillis();
 		System.out.println("The program took " + (endTime - startTime) + " ms to compile."); 
 
